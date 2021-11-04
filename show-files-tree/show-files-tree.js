4c0ae38b71fcc728f82a4e2750c2a5ce5121f5d6
jugosoft <sardimka@gmail.com>
2021-11-04 17:26:16 +0400

    final bug fix, correct output

--- show-files-tree/show-files-tree.js
+++ show-files-tree/show-files-tree.js
@@ -5,11 +5,12 @@ exports.showFilesTree = function showFilesTree(dirname, offset = 0) {
     const offsetString = ' '.repeat(offset) + '└' + '─';
     const files = fs.readdirSync(dirname);
     for (const file in files) {
-        console.log(offsetString, files[file]);
-        if (fs.lstatSync(`${dirname}\\${files[file]}`).isDirectory()) {
-            showFilesTree(`${dirname}\\${files[file]}`, offset + 1);
+        const element = `${dirname}\\${files[file]}`;
+        if (fs.lstatSync(element).isDirectory()) {
+            console.log(offsetString, files[file]);
+            showFilesTree(element, offset + 1);
         } else {
-            console.log(`${dirname}\\${files[file]}`, offset + 1);
+            console.log(offsetString, files[file]);
         }
     }
 }
